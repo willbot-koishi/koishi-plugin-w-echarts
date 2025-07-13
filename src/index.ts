f36879cf2b833f892b5b0b96f946b49bd8147bb0
ForkΨKILLET <fork_killet@outlook.com>
2025-07-13 12:52:46 +0800

    refa: use more common canvas service API

--- src/index.ts
+++ src/index.ts
@@ -7,75 +7,93 @@ import * as echarts from 'echarts'
 export const name = 'w-echarts'
 
 declare module 'koishi' {
-    interface Context {
-        echarts: EChartService
-    }
+  interface Context {
+    echarts: EChartService
+  }
 }
 
 export type EChartHandler = {
-    chart: echarts.ECharts
-    canvas: skia.Canvas
-    dispose: () => void
-    export: (timeout?: number) => Promise<h>
+  chart: echarts.ECharts
+  canvas: skia.Canvas
+  dispose: () => void
+  export: (timeout?: number) => Promise<h>
 }
 
 type RemoveIndex<T> = {
-    [K in keyof T as
-        string extends K ? never :
-        number extends K ? never :
-        symbol extends K ? never :
-        K
-    ] : T[K]
+  [K in keyof T as
+    string extends K ? never :
+    number extends K ? never :
+    symbol extends K ? never :
+    K
+  ] : T[K]
 }
 
 export type StrictEChartsOption = RemoveIndex<echarts.EChartsOption>
 
 class EChartService extends Service {
-    static readonly inject = [ 'canvas' ]
+  static readonly inject = [ 'canvas' ]
 
-    public createChart<Strict extends boolean = false>(
-        width: number,
-        height: number,
-        options: Strict extends true ? StrictEChartsOption : echarts.EChartsOption
-    ): EChartHandler {
-        const canvas = new this.ctx.canvas.skia.Canvas(width, height)
-        const chart = echarts.init(canvas as any)
-        chart.setOption({
-            textStyle: {
-                fontFamily: this.config.font
-            },
-            ...options
+  public createChart<Strict extends boolean = false>(
+    width: number,
+    height: number,
+    options: Strict extends true ? StrictEChartsOption : echarts.EChartsOption
+  ): EChartHandler {
+    const canvas = new this.ctx.canvas.Canvas(width, height)
+    const chart = echarts.init(canvas as unknown as HTMLElement)
+    chart.setOption({
+      textStyle: {
+        fontFamily: this.config.font
+      },
+      ...options
+    })
+    return {
+      chart,
+      canvas,
+      dispose: () => chart.dispose(),
+      export: async (timeout = 2000) => {
+        await new Promise<void>(res => {
+          chart.on('finished', () => res())
+          setTimeout(() => res(), timeout)
         })
-        return {
-            chart,
-            canvas,
-            dispose: () => chart.dispose(),
-            export: async (timeout = 2000) => {
-                await new Promise<void>(res => {
-                    chart.on('finished', () => res())
-                    setTimeout(() => res(), timeout)
-                })
-                const el = h.image(await canvas.toBuffer('png'), 'image/png')
-                chart.dispose()
-                return el
-            }
-        }
+        const el = h.image(await canvas.toBuffer('png'), 'image/png')
+        chart.dispose()
+        return el
+      }
     }
+  }
 
-    public constructor(ctx: Context, public config: EChartService.Config) {
-        super(ctx, 'echarts')
+  public constructor(ctx: Context, public config: EChartService.Config) {
+    super(ctx, 'echarts')
 
-        this.ctx.command('echarts', 'ECharts 服务')
-    }
+    this.ctx.command('echarts', 'ECharts service')
+
+    this.ctx.command('echarts.test', 'Test ECharts service. Returns a simple pie chart.')
+      .action(async () => {
+        const handler = this.createChart(400, 300, {
+          title: { text: 'ECharts Test' },
+          tooltip: {},
+          series: {
+            type: 'pie',
+            data: [
+              { value: 25, name: 'A' },
+              { value: 25, name: 'B' },
+              { value: 25, name: 'Cheeze' },
+              { value: 25, name: 'D' },
+            ]
+          }
+        })
+        return handler.export()
+      })
+  }
 }
 
 namespace EChartService {
-    export interface Config {
-        font: string
-    }
-    export const Config: z<Config> = z.object({
-        font: z.string().default('sans').description('ECharts 使用的字体')
-    })
+  export interface Config {
+    font: string
+  }
+  export const Config: z<Config> = z.object({
+    font: z.string().default('sans').description('ECharts 使用的字体')
+  })
 }
 
 export default EChartService
